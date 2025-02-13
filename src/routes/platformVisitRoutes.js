71d918ac9faff7700e8cea185bb3a9e27773b035
Mofeyyy <jahzelpatdu021@gmail.com>
2025-02-13 15:15:00 +0800

    feat: update models and routes

--- src/routes/platformVisitRoutes.js
+++ src/routes/platformVisitRoutes.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const {
-  trackVisit,
-  getPlatformVisits,
-} = require("../controllers/platformVisitController");
+const { trackVisit, getPlatformVisits } = require("../controllers/platformVisitController");
+//const requireAuth = require("../middlewares/requireAuth");
+//const requireRole = require("../middlewares/requireRole");
 
-router.post("/", trackVisit); // Track Guest/User Visits
-router.get("/", getPlatformVisits); // Get Platform Visit Statistics (Admin Dashboard)
+// Track a guest/user visit
+router.post("/track", trackVisit);
+
+// Get platform visit statistics (Admin Only)
+//router.get("/", requireAuth, requireRole(["admin"]), getPlatformVisits); for authentication
+router.get("/", getPlatformVisits);
 
 module.exports = router;
