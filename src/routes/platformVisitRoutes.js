ed83550bdaab81631ab78e42c27ee2dea5c35669
Mofeyyy <jahzelpatdu021@gmail.com>
2025-03-05 15:50:53 +0800

    feat: update platform visit controller/model/routes/validations

--- src/routes/platformVisitRoutes.js
+++ src/routes/platformVisitRoutes.js
@@ -6,9 +6,11 @@ import {
   getPlatformVisits,
 } from "../controllers/platformVisitController.js";
 
+import authenticateUser from "../middleware/authenticateUser.js";
+
 const router = express.Router();
 
-router.post("/", trackVisit); // Track Guest/User Visits
+router.post("/", authenticateUser.optional, trackVisit); // Track Guest/User Visits
 router.get("/", getPlatformVisits); // Get Platform Visit Statistics (Admin Dashboard)
 
 export default router;
